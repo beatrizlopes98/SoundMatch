d827a29d1839685c14c08ff2780c318af9531734
Alfonso Villanueva <alfonso-40200365@esmad.ipp.pt>
2024-01-15 17:57:28 +0000

    add logs

--- SoundMatch-API/Controllers/userController.js
+++ SoundMatch-API/Controllers/userController.js
@@ -9,6 +9,7 @@ const saltRounds = 10;
 exports.getUserProfile = async function (req, res) {
   try {
     const user = await users.findOne({ email: req.user });
+    console.log(req.user)
 
     if (!user) {
       return handleError(res, 404, "User not found");
@@ -31,6 +32,7 @@ exports.getUserProfile = async function (req, res) {
 exports.editUserProfile = async function (req, res) {
   try {
     const { name, password } = req.body;
+    console.log(req.user)
 
     const user = await users.findOne({ email: req.user });
 
