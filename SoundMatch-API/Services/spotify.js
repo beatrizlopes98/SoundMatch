647fd4e51b8b5fb111af22e4f8f3dbb1b6a22e50
Alfonso Villanueva <alfonso-40200365@esmad.ipp.pt>
2024-01-16 22:08:41 +0000

    get spotify playlit by id and upload new musics to table

--- SoundMatch-API/Services/spotify.js
+++ SoundMatch-API/Services/spotify.js
@@ -21,6 +21,29 @@ const getPlaylists = async (accessToken) => {
   }
 };
 
+const getPlaylistById = async (playlistId, accessToken) => {
+  try {
+    const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to get playlist by ID: ${response.statusText}`);
+    }
+
+    const playlistData = await response.json();
+    return playlistData;
+  } catch (error) {
+    throw new Error(`Error getting playlist by ID: ${error.message}`);
+  }
+};
+
+
 module.exports = {
+  getPlaylistById,
   getPlaylists,
 };
